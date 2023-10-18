82eeccfb47c80fc7b65fb8d35f4e87ebbb8c7b6c
Arhion71 <113120262+Arhion71@users.noreply.github.com>
2023-10-18 16:32:43 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -24,4 +24,27 @@ $(document).ready(function () {
       disableOnInteraction: false,
     },
   });
+
+  const swipeReviewSlide = new Swiper(".review_slider__swiper", {
+    slidesPerView: 3,
+    loop: true,
+    spaceBetween: 14,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
+$(".main_catalog .select").on("click", function (e) {
+  if ($(e.target).is(".select__item")) {
+    e.preventDefault();
+    return;
+  }
+  $(this).find(".select__list").toggle();
+});
+
+$(".main_catalog .select__item").on("click", function () {
+  var $selectList = $(this).parents(".select__list");
+  var $selectSelected = $selectList.siblings(".select__selected");
+  var $selectKey = $selectSelected.attr("data-filter-name");
 });
